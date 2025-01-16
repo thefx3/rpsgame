7cf0f00bd52bd3401bae058e10a8b2d4f0c16820
thefx3 <fx.pairault@yahoo.fr>
2025-01-16 20:07:59 +0100

    Write computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+function getComputerChoice(){
+    //Return rock - paper - scissor randomly
+    choice = Math.random()*3
+    // console.log(choice); // Give a number between 1 and 3. 
+
+    if (choice > 0 && choice <=1){
+        return "Rock";
+    }
+    else if (choice >1 && choice <=2){
+        return "Paper";
+    }
+    else if (choice >2 && choice <=3){
+        return "Scissors";
+    }
+}
