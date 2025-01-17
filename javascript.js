145fd2971b110a1af0a098ededdbfe67aca3d064
thefx3 <fx.pairault@yahoo.fr>
2025-01-17 17:13:01 +0100

    Create the score and the playround function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice(){
-    //Return rock - paper - scissor randomly
     choice = Math.random()*3
-    // console.log(choice); // Give a number between 1 and 3. 
 
     if (choice > 0 && choice <=1){
         return "Rock";
@@ -16,6 +18,40 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     choice = prompt("Rock ? Paper ? Scissors ?");
-    console.log(choice);
     return choice; 
 }
+
+
+function playRound(humanChoice,computerChoice){
+
+    let humanPoint = 0;
+    let computerPoint = 0;
+    // humanChoice will ask the human to choose
+        //Human choice can write anything - but only valid answers and case-insensitive
+        //Not take care of lower/uppercase
+    //computerChoice will generate a random choice
+    console.log(humanSelection + " is human choice");
+    console.log(computerSelection + " is computer choice");
+
+    if (computerChoice==humanChoice){
+        console.log("It's a draw!");
+    }
+    else if ((humanChoice=="Paper" && computerChoice=="Rock")||
+       (humanChoice=="Rock" && computerChoice=="Scissors")||
+       (humanChoice=="Scissors" && computerChoice=="Paper")){
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+        humanPoint = humanPoint +1;
+    }
+     else if ((humanChoice=="Rock" && computerChoice=="Paper")||
+       (humanChoice=="Scissors" && computerChoice=="Rock")||
+       (humanChoice=="Paper" && computerChoice=="Scissors")){
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computerPoint = computerPoint + 1;
+    } 
+    console.log("Human : " + humanPoint + " | " + computerPoint + " Computer");
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
